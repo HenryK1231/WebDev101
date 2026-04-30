e34d6e93750224c636c08091192141a2b2e13dd1
Henry <hkrussel1@my.stlcc.edu>
2026-04-30 09:34:26 -0500

    h

--- WordCount/word-counter.js
+++ WordCount/word-counter.js
@@ -1,6 +1,8 @@
 <script>
-  document.querySelector("Hello").innerText = "Hello World";
-  document.split("Hello").length;
-  let words = "Hello World";
-  words / 250;
+let $P = document.querySelector('p');
+let $TEXT = $P.innerText;
+let $LIST = $TEXT.split(" "); 
+let $WORDS = $LIST.length;
+let $TIME = $WORDS / 250;
+$P.innerText += ...;
 </script>
